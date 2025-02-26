993bd34793996e3b90d7928adbb7a1567b74c00f
CarolMoreti <carolline.moreti1bmangal@gmail.com>
2025-02-26 18:18:53 +0000

    .

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,15 +12,45 @@ menuOpener.addEventListener('click', ()=>{
         menuOpener.querySelector('.close-icon').style.display='flex';
         menuOpener.querySelector('.hamburguer-icon').style.display='none';
     }
-})
+});
 
 
 
 
 //testemonials
-let testemonials =[
-    {quote:'"Mais do que nunca, os animais são tratados como membros da família"', origin:'cbs.svg' },
+let testemonials =[{quote:'"Mais do que nunca, os animais são tratados como membros da família"', origin:'cbs.svg' },
     {quote:'"DogDev é um serviço de entrega direto ao consumidor, preparado na hora com ingredientes 100% reais. Ingredientes que humanos reconheceriam"', origin:'forbes.svg' }, 
     {quote:'"DogDev usa ingredientes simples e limpos em seus produtos"', origin:'fox.svg' }, 
     {quote:'"Vejo você DogDev como um verdadeiro herói"', origin:'sharktank.svg' }
 ];
+let testemonialQuote = document.querySelector('.testemonials .quote')
+let testemonialIcon = document.querySelector('.testemonials .icons')
+
+for(let tindex in testemonials){
+    let img = document.createElement('img')
+    img.setAttribute('src','./assets/images/'+testemonials[parseInt(tindex)].origin)
+    img.style.cursor='pointer'
+    img.addEventListener('click', ()=>fillTestemonial(parseInt(tindex)))
+    testemonialIcon.appendChild(img)
+}
+
+let currentTestemonial = 0;
+let testemonialTimer;
+
+const fillTestemonial= (index)=>{
+    clearTimeout(testemonialTimer)
+    currentTestemonial = index
+    testemonialQuote.innerHTML = testemonials[currentTestemonial].quote
+    let imgs = testemonialIcon.querySelectorAll('img')
+    for(let img of imgs) img.style.opacity=0.2
+    imgs[currentTestemonial].style.opacity=1
+    testemonialTimer = setTimeout(nextTestemonial,3000)
+}
+const nextTestemonial = () =>{
+    if (testemonials[currentTestemonial]+1){
+        fillTestemonial(currentTestemonial+1)
+    }else{
+        fillTestemonial(0)
+    }
+}
+nextTestemonial();
