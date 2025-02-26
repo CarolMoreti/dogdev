6debe7ee14e0f7393cb3ae68c84e3dd4ff550f8f
CarolMoreti <carolline.moreti1bmangal@gmail.com>
2025-02-26 15:25:05 +0000

    .

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+//menu hamburguer
 let menuOpener= document.querySelector('.menu-opener')
 let nav = document.querySelector('header nav')
 
@@ -12,3 +13,14 @@ menuOpener.addEventListener('click', ()=>{
         menuOpener.querySelector('.hamburguer-icon').style.display='none';
     }
 })
+
+
+
+
+//testemonials
+let testemonials =[
+    {quote:'"Mais do que nunca, os animais são tratados como membros da família"', origin:'cbs.svg' },
+    {quote:'"DogDev é um serviço de entrega direto ao consumidor, preparado na hora com ingredientes 100% reais. Ingredientes que humanos reconheceriam"', origin:'forbes.svg' }, 
+    {quote:'"DogDev usa ingredientes simples e limpos em seus produtos"', origin:'fox.svg' }, 
+    {quote:'"Vejo você DogDev como um verdadeiro herói"', origin:'sharktank.svg' }
+];
